4a103a4ac2ce7284e59013ae494173990d280e9e
Joe O'Hara <joe.is.ohara@gmail.com>
2017-02-28 00:34:49 +0000

    Tidy up of parser

--- Insightful/Parser/ReviewParser.php
+++ Insightful/Parser/ReviewParser.php
@@ -1,13 +1,121 @@
 <?php
+namespace Insightful\Parser;
+
+use GuzzleHttp\Client;
+
 /**
  * Created by PhpStorm.
  * User: joe
  * Date: 27/02/2017
  * Time: 17:33
  */
-
-
 class ReviewParser
 {
+    const CSAPI_URL = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/";
+    const API_ENV_KEY = "MS_API_KEY";
 
+    private static function buildDocument(string $reviewDocument, string $id) : array
+    {
+        $document = [];                    //create document
+        $document['language'] = 'en';
+        $document['id'] = (string)$id;
+        $document['text'] = $reviewDocument;
+
+        return $document;
+    }
+
+    private static function getHeaders() : array
+    {
+        //header necessary for every post request
+        return [
+            'Ocp-Apim-Subscription-Key' => env(self::API_ENV_KEY),
+            'Content-Type'              => 'application/json',
+            'Accept'                    => 'application/json'
+        ];
+    }
+
+    // Splits a sentence smartly
+    // http://stackoverflow.com/questions/16377437/split-a-text-into-sentences
+    private static function splitToSentence(string $string) : array
+    {
+        return preg_split('/(?<=[.?!])\s+(?=[a-z])/i', $string);
+    }
+
+
+    //Takes a review and splits it into documents with incrementing IDs
+    private static function splitIntoDocuments(string $text, int $index) : array
+    {
+        $sentenceOffset = 0;
+
+        // Index documents by the Id of the review and a decimal, eg 1.0,1.1...1.10
+        $sentences = self::splitToSentence($text);        //split by sentence
+        $documents = [];
+        foreach ($sentences as $sentence) {
+            $id = (string)$index . "." . $sentenceOffset++;
+            $documents[] = self::buildDocument($sentence, $id);
+        }
+
+        return $documents;
+    }
+
+
+    /**
+     *
+     * @param array $reviews Array of \Insight\Review objects
+     * @return array Returns Array of arrays of sentiment and phrases
+     *                       Returns Array of arrays of sentiment and phrases
+     * @internal param array $review_texts String array of review text
+     */
+    public static function parseReviews(array $reviews) : array
+    {
+
+        $numReviews = sizeof($reviews);
+        //Don't for-see this happening soon. Eventually below could will need to be split to batch based on 10k Document limit
+        if ($numReviews > 3000) {
+            echo "Fatal Error: More than 3000 (" . $numReviews . ")reviews being parsed into one call. Entries may be lost due to API limits";
+            die();
+        }
+
+
+        $client = new Client(['base_uri' => self::CSAPI_URL]);
+
+
+        $documents = [];
+        foreach ($reviews as $review) {            //each full review
+            $documents[] = self::splitIntoDocuments($review->text, $review->id);
+        }
+
+        //Collection of Documents to be sent
+        $body = [                                            //body of API call
+            "documents" => $documents
+        ];
+
+        try{
+            $phrase_response = $client->post('keyPhrases/', [    //key phrases API call
+                'headers' => self::getHeaders(),
+                'json'    => $body
+            ]);
+
+            $sentiment_response = $client->post('sentiment/', [    //sentiment API call
+                'headers' => self::getHeaders(),
+                'json'    => $body
+            ]);
+
+            $sentiment = json_decode($sentiment_response->getBody(), true);//decode response to array that can be parsed
+            $phrases = json_decode($phrase_response->getBody(), true);
+
+            $sentiments = $sentiment['documents'];
+            $keyPhrases = $phrases['documents'];
+
+
+            return ['sentiment' => $sentiments, 'phrases' => $keyPhrases];
+
+        } catch (\GuzzleHttp\Exception\ClientException $e){
+            //Catch error - print it and throw again for command line to catch
+            $failMessage = $e->getResponse()->getBody()->getContents();
+            $decoded = json_decode($failMessage, true);
+            var_dump($decoded);
+            throw $e;
+        }
+    }
 }
--- app/Console/Commands/ParseReviews.php
+++ app/Console/Commands/ParseReviews.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Insightful\Parser\ReviewParser;
+use Insightful\RetrievedReview;
+use Insightful\Review;
 
 class ParseReviews extends Command
 {
@@ -37,6 +40,32 @@ class ParseReviews extends Command
      */
     public function handle()
     {
-        //
+        $review_texts[] = "From the awesome greeting by Drew, to the wine and cheese happy hour, nice accommodations and coffee in the morning, we couldn't have been happier with our stay. It was very convenient to walk to MSG and Times Square with so many restaurants and bars to choose from. The courtyard was kind of packed up for the winter but we still enjoyed a cocktail during a unseasonably warm weekend for NYC.";
+        $review_texts[] = 'Me and my wife wanted to plan a special weekend in NYC. We usually stay at Ritz in Central Park or Battery Park being the service is always over the top. When searching TripAdvisor we found some Great Reviews of the 414 Hotel. So I took the chance and decided to investigate further. Well let me tell you the experience of this property was OVER THE TOP AND THEN SOME. I already knew from the initial phone call and chat room experience that we were making the right choice. They were extremely accommodating providing restaurants to dine and things to do and all of this was done in an e-mail immediately after making the reservation.
+						Once we arrived the hospitality was Exceptional to say the least it was like being greeted by family and they also knew it was my wife\'s Birthday and had a special surprise in the room that would have cost over $100 anywhere else. The morning Breakfast was nice and the Happy Hour time with Fireplace and interaction with other guest was also enjoyable.
+						This location is hidden and a must stay for those that want service with a smile and then some. We liked it so much that we decided that whenever we stay in the city in the future we will call 414 Hotel our home as that is how they made us feel during the entire visit. Great Staff, Clean Rooms and Great Location.';
+
+        $review1 = new RetrievedReview();
+        $review2 = new RetrievedReview();
+        $review1->text = $review_texts[0];
+        $review2->text = $review_texts[1];
+        $review1->id = 1;
+        $review2->id = 2;
+
+        $reviewObjects = [
+            $review1,
+            $review2
+        ];
+
+        /*
+         *  Get all reviews as Eloquent model - where not parsed
+         *  Use same result object to update parsed to true
+         * */
+
+
+
+        $res = ReviewParser::parseReviews($reviewObjects);
+
+        print_r($res);
     }
 }
